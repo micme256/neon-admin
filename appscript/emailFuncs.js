c7148cb1f04a1860c21f0ab1eb00454ee48efff8
micme256 <nshimye.michael.2019@gmail.com>
2025-05-02 09:27:51 +0300

    Added logic to send loan mobilisation emails

---
+++ appscript/emailFuncs.js
@@ -0,0 +1,67 @@
+const sendEmails = (subject, body, email) => {
+  MailApp.sendEmail({
+    to: email,
+    subject: subject,
+    htmlBody: body,
+  });
+};
+
+const emailsById = (memberId) => {
+  const memberInfo = getMemberInfoById(memberId);
+  return memberInfo ? memberInfo.email : null;
+};
+
+const nameById = (memberId) => {
+  const memberInfo = getMemberInfoById(memberId);
+  return memberInfo ? memberInfo.name : null;
+};
+
+const emailsByPost = (position) => {
+  const membersMap = membersInfoMap();
+  const emails = [];
+  for (const [, memberData] of membersMap.entries()) {
+    if (memberData.position === position) {
+      emails.push(memberData.email);
+    }
+  }
+  return emails;
+};
+const emailsByExcludedPost = (excludedPosition) => {
+  const membersMap = membersInfoMap();
+  const emails = [];
+  for (const [, memberData] of membersMap.entries()) {
+    if (memberData.position !== excludedPosition) {
+      emails.push(memberData.email);
+    }
+  }
+  return emails;
+};
+
+const getMemberInfoById = (memberId) => {
+  const membersMap = membersInfoMap();
+  return membersMap.get(memberId);
+};
+const membersInfoMap = () => {
+  const sheetName = "Database";
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+  const data = sheet.getDataRange().getValues();
+  const headers = data[0];
+
+  const memberIdIndex = headers.indexOf("No/ID");
+  const nameIndex = headers.indexOf("first Name");
+  const positionIndex = headers.indexOf("Position");
+  const emailIndex = headers.indexOf("Email ID");
+
+  const memberMap = new Map();
+  for (let i = 1; i < data.length; i++) {
+    const row = data[i];
+    const memberId = row[memberIdIndex];
+    const memberDetails = {
+      name: row[nameIndex],
+      position: row[positionIndex],
+      email: row[emailIndex],
+    };
+    memberMap.set(memberId, memberDetails);
+  }
+  return memberMap;
+};
---
+++ appscript/helperFuncs.js
@@ -0,0 +1,56 @@
+//Helper function to check sheet and return data
+const checkSheetData = (sheetName) => {
+  try {
+    const sheet =
+      SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+    if (!sheet) {
+      throw new Error(`Transaction '${sheetName}' not found`);
+    }
+    const data = sheet.getDataRange().getValues();
+    return { data, sheet };
+  } catch (error) {
+    return error;
+  }
+};
+
+const namesMapedById = () => {
+  const sheetName = "Database";
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+
+  const data = sheet.getDataRange().getValues();
+  const memberMap = new Map();
+
+  for (let i = 0; i < data.length; i++) {
+    const memberId = data[i][0];
+    const memberName = data[i][2];
+    memberMap.set(memberId, memberName);
+  }
+
+  return memberMap;
+};
+
+//Helper function to generate transaction IDs
+const generateTransactionId = (prefix) => {
+  const date = new Date();
+  const year = date.getFullYear().toString().slice(-2);
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const randomPart = Math.random().toString(36).slice(2, 7).toUpperCase();
+  return `${prefix}${year}${month}${day}-${randomPart}`;
+};
+
+//Convert stringfied dates to date
+const convertToDate = (input) => {
+  if (input instanceof Date) return input; // Already valid
+  if (typeof input !== "string") return null;
+
+  const parts = input.split("/");
+  if (parts.length !== 3) return null;
+
+  const day = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1;
+  const year = parseInt(parts[2], 10);
+
+  const date = new Date(year, month, day);
+  return isNaN(date.getTime()) ? null : date;
+};
