bae0aa2766a804a260371446e21ef1a2869846d3
micme256 <nshimye.michael.2019@gmail.com>
2025-05-11 20:39:26 +0300

    Re-organizing flies, nothing added

--- appscript/helperFuncs.js
+++ appscript/helperFuncs.js
@@ -1,23 +1,6 @@
 //Helper function to check sheet and return data
-const checkSheetData = (sheetName) => {
-  try {
-    const sheet =
-      SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
-    if (!sheet) {
-      throw new Error(`Transaction '${sheetName}' not found`);
-    }
-    const data = sheet.getDataRange().getValues();
-    return { data, sheet };
-  } catch (error) {
-    return error;
-  }
-};
-
 const namesMapedById = () => {
-  const sheetName = "Database";
-  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
-
-  const data = sheet.getDataRange().getValues();
+  const { data } = checkSheetData("Database");
   const headers = data[0];
 
   const memberIdIndex = headers.indexOf("No/ID");
@@ -32,6 +15,20 @@ const namesMapedById = () => {
   }
 
   return memberMap;
+};
+
+const checkSheetData = (sheetName) => {
+  try {
+    const sheet =
+      SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName);
+    if (!sheet) {
+      throw new Error(`Transaction '${sheetName}' not found`);
+    }
+    const data = sheet.getDataRange().getValues();
+    return { data, sheet };
+  } catch (error) {
+    return error;
+  }
 };
 
 //Helper function to generate transaction IDs
